688ab76efbeda749a5c10850713ef05c4e50ae06
Glendon Chin <glendonchin@gmail.com>
2022-06-09 11:20:39 -0400

    home nav bar, footer, favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './bootstrap.min.css'
+import { Container } from 'react-bootstrap'
 function App() {
   return (
     <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Hello!</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
